08aeeaa9089104e7dabe9e42db925b399ade0c2a
Dennis Liu <liusx880630@gmail.com>
2022-05-27 09:18:34 +0800

    Modify part of installation methods after test
    Add readme.md

--- OS/Ubuntu/Ubuntu_packages.sh
+++ OS/Ubuntu/Ubuntu_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Author: DennisLiu16
 
-# Version: 1.0
+# Version: 1.1
 
 # For any server, the best way to get this script is git clone
 # sudo apt install git 
@@ -44,12 +44,22 @@ sudo apt upgrade -y
 # make
 installation_info "build-essential"
 sudo apt install build-essential -y
+sudo apt install libssl-dev -y
+sudo apt install gdb -y
 installation_end "build-essential"
 
 # install development tools
-installation_info "cmake"
-sudo apt install cmake -y
-installation_end "cmake"
+installation_info "cmake 3.22.4"
+# Could NOT find OpenSSL, try to set the path to OpenSSL root folder problem
+sudo apt install libssl-dev
+# CMake 3.22.4
+wget https://github.com/Kitware/CMake/releases/download/v3.22.4/cmake-3.22.4.tar.gz
+tar zxvf cmake-3.22.4.tar.gz
+cd cmake-3.22.4
+./bootstrap
+make
+sudo make install
+installation_end "cmake 3.22.4"
 
 installation_info "git"
 sudo apt install git -y
@@ -60,9 +70,14 @@ installation_info "vim"
 sudo apt install vim
 installation_end "vim"
 
+# TODO:check valid or not
 installation_info "code"
 sudo snap install --classic code
 installation_end "code"
+
+installation_info "pip"
+sudo apt install python3-pip -y
+installation_end "pip"
 
 # install network related
 installation_info "nmap"
@@ -74,7 +89,7 @@ installation_info "tree"
 sudo apt install tree
 installation_end "tree"
 
-# install some libraries for project - LRA
+# install some libraries for project - LRA on Raspberry 4b
 # C++ fmt
 installation_info "fmt"
 # TODO:check fmt existed or not
@@ -86,18 +101,33 @@ then
     cd build
     cmake ..
     sudo make install
+    cd ../..
 fi
 installation_end "fmt"
 
 # wiringPi
 installation_info "wiringPi"
-sudo apt install wiringpi -y
+# Oops model 17 problem
+sudo dpkg --add-architecture armhf
+sudo apt update
+wget https://github.com/guation/WiringPi-arm64/releases/download/2.61-g/wiringpi-2.61-g.deb
+sudo apt install -f ./wiringpi-*-g.deb
+
+# gpio -readall group problem
+sudo apt install rpi.gpio-common
+sudo adduser $(USER) dialout
+
 installation_end "wiringPi"
+
+# install libi2c
+installation_info "libi2c-dev"
+sudo apt install libi2c-dev -y
+installation_end "libi2c-dev"
 
 # vcgencmd
 installation_info "vcgencmd"
 sudo apt install libraspberrypi-bin -y
-sudo usermod -aG video $(whoami)
+sudo usermod -aG video $(USER)
 installation_end "vcgencmd"
 
 # main program
@@ -110,6 +140,8 @@ cd ~
 git clone https://github.com/DennisLiu16/LRA_Raspberry4b.git
 
 installation_end "LRA main program"
+
+# TODO:add global and git alias
 
 # TODO:if reboot flag on
 sudo reboot
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Scripts for development tools installation 
+
+## V1
+- OS
+    - only Ubuntu on Raspberry 4b
+
+## How to use
+1. **Download repo**
+
+    ```
+    git clone https://github.com/DennisLiu16/pkg_installation_script.git
+    ```
+
+2. Find script named **Ubuntu_packages.sh**
+
+3. Execute the script
+
+    ```
+    ./Ubuntu_packages.sh
+    ```
